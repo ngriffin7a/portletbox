317bfb2d97a6571cbf9e01dfb5f95610f2c141ba
Neil Griffin <neil.griffin.scm@gmail.com>
2013-05-28 09:43:04 -0400

    Merged contributions for PORTLETSPEC3-10 from Scott Nicklous

--- issues/PORTLETSPEC3-10-portlet/src/main/java/com/liferay/portletbox/TestPortlet.java
+++ issues/PORTLETSPEC3-10-portlet/src/main/java/com/liferay/portletbox/TestPortlet.java
@@ -15,9 +15,12 @@ package com.liferay.portletbox;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
+import java.util.Set;
 
 import javax.portlet.GenericPortlet;
 import javax.portlet.PortletException;
+import javax.portlet.PortletRequest;
 import javax.portlet.PortletURL;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
@@ -41,20 +44,39 @@ public class TestPortlet extends GenericPortlet {
 
 		PrintWriter writer = resourceResponse.getWriter();
 		writer.write("<html><body>");
+
+		writer.write(HTMLUtil.HR_TAG);
+		HTMLUtil.writeMapCompact(writer, PortletRequest.RESOURCE_PHASE, "publicParameterMap",
+			resourceRequest.getPublicParameterMap());
+		writer.write(HTMLUtil.HR_TAG);
+		HTMLUtil.writeMapCompact(writer, PortletRequest.RESOURCE_PHASE, "privateParameterMap",
+			resourceRequest.getPrivateParameterMap());
+		writer.write(HTMLUtil.HR_TAG);
+		HTMLUtil.writeMapCompact(writer, PortletRequest.RESOURCE_PHASE, "parameterMap",
+			resourceRequest.getParameterMap());
+		writer.write(HTMLUtil.HR_TAG);
+		HTMLUtil.writeParameters(writer, PortletRequest.RESOURCE_PHASE, resourceRequest);
+		writer.write(HTMLUtil.HR_TAG);
+
 		writer.write("<span>");
-		writer.write("resourceURL=<br />");
+
+		// Create resource URL with no further processing -
 
 		ResourceURL resourceURL = resourceResponse.createResourceURL();
-		resourceURL.setParameter("publicRenderParameter1", "4");
 
-		String resourceURLAsString = resourceURL.toString();
-		writer.write("The following URL is a result of calling renderResponse.createResourceURL()");
-		writer.write(HTMLUtil.BR_TAG);
-		writer.write("<a href=\"");
-		writer.write(resourceURLAsString);
-		writer.write("\">");
-		writer.write(resourceURLAsString);
-		writer.write("</a>");
+		HTMLUtil.writeURL(writer,
+			"The following URL is a result of calling resourceResponse.createResourceURL() without setting a new parameter:",
+			resourceURL.toString());
+
+		// Create resource URL, setting parameter -
+
+		resourceURL = resourceResponse.createResourceURL();
+		resourceURL.setParameter("resourceURLParameter4", "44");
+
+		HTMLUtil.writeURL(writer,
+			"The following URL is a result of calling resourceResponse.createResourceURL() and setting a new parameter:",
+			resourceURL.toString());
+
 		writer.write("</span>");
 		writer.write("</body></html>");
 	}
@@ -65,7 +87,17 @@ public class TestPortlet extends GenericPortlet {
 
 		PrintWriter writer = renderResponse.getWriter();
 
-		String renderURLAsString = renderResponse.createRenderURL().toString();
+		writer.write(HTMLUtil.HR_TAG);
+		HTMLUtil.writeMapCompact(writer, PortletRequest.RENDER_PHASE, "publicParameterMap",
+			renderRequest.getPublicParameterMap());
+		writer.write(HTMLUtil.HR_TAG);
+		HTMLUtil.writeMapCompact(writer, PortletRequest.RENDER_PHASE, "privateParameterMap",
+			renderRequest.getPrivateParameterMap());
+		writer.write(HTMLUtil.HR_TAG);
+		HTMLUtil.writeMapCompact(writer, PortletRequest.RENDER_PHASE, "parameterMap", renderRequest.getParameterMap());
+		writer.write(HTMLUtil.HR_TAG);
+		HTMLUtil.writeParameters(writer, PortletRequest.RENDER_PHASE, renderRequest);
+
 		writer.write(HTMLUtil.HR_TAG);
 		writer.write("renderRequest.getParameter(\"publicRenderParameter1\")=[");
 
@@ -77,47 +109,162 @@ public class TestPortlet extends GenericPortlet {
 
 		writer.write(publicRenderParameter1);
 		writer.write("]");
+		writer.write(HTMLUtil.HR_TAG);
+
+		// Create render URL w/o parameters -
 
 		PortletURL renderURL = renderResponse.createRenderURL();
+
+		HTMLUtil.writeURL(writer, "Created render URL w/o parameters -", renderURL.toString());
+
+		// Create Render URL, copying all parameters from request using map -
+
+		renderURL = renderResponse.createRenderURL();
+
+		Map<String, String[]> parmMap = renderRequest.getParameterMap();
+		renderURL.setParameters(parmMap);
+
+		HTMLUtil.writeURL(writer, "Create Render URL, copying all parameters from request using map -",
+			renderURL.toString());
+
+		// set public & private render parameters -
+
+		renderURL = renderResponse.createRenderURL();
+
 		renderURL.setParameter("publicRenderParameter1", "2");
-		renderURLAsString = renderURL.toString();
+		renderURL.setParameter("privateRenderParameter1", "1");
 
-		writer.write(HTMLUtil.HR_TAG);
-		writer.write(
-			"The following URL is a result of calling renderResponse.createRenderURL() and then setParameter(\"publicRenderParameter1\", \"2\")");
-		writer.write(HTMLUtil.BR_TAG);
-		writer.write("<a href=\"");
-		writer.write(renderURLAsString);
-		writer.write("\">");
-		writer.write(renderURLAsString);
-		writer.write("</a>");
-		writer.write(HTMLUtil.HR_TAG);
+		HTMLUtil.writeURL(writer,
+			"The following URL is a result of calling renderResponse.createRenderURL() and then setParameter(\"publicRenderParameter1\", \"2\") and setParameter(\"privateRenderParameter1\", \"1\")",
+			renderURL.toString());
 
-		String resourceURLAsString = renderResponse.createResourceURL().toString();
-		writer.write(HTMLUtil.HR_TAG);
-		writer.write("The following URL is a result of calling renderResponse.createResourceURL()");
-		writer.write(HTMLUtil.BR_TAG);
-		writer.write("<a href=\"");
-		writer.write(resourceURLAsString);
-		writer.write("\">");
-		writer.write(resourceURLAsString);
-		writer.write("</a>");
-		writer.write(HTMLUtil.HR_TAG);
+		// set public & private render parameters, making use of map -
+
+		renderURL = renderResponse.createRenderURL();
+
+		renderURL.setParameter("publicRenderParameter1", "2");
+		renderURL.setParameter("privateRenderParameter1", "1");
+
+		String[] values = { "Fred", "Wilma", "Pebbles" };
+		renderURL.getParameterMap().put("privateRenderParameter2", values);
+
+		HTMLUtil.writeURL(writer,
+			"The following URL is a result of calling renderResponse.createRenderURL(), setting public and private parameters, and getParameterMap().put()",
+			renderURL.toString());
+
+		// set & delete parameters on render URL through setParameter() -
+
+		renderURL = renderResponse.createRenderURL();
+
+		renderURL.setParameter("privateRenderParameter1", "1");
+
+		Set<String> keySet = parmMap.keySet();
+
+		for (String key : keySet) {
+
+			try {
+				renderURL.setParameter(key, (String) null);
+			}
+			catch (Exception e) {
+				writer.write("remove " + key + " from URL failed.<br/>" + e.getMessage() + "<br/>");
+			}
+		}
+
+		HTMLUtil.writeURL(writer,
+			"The following URL is a result of calling renderResponse.createRenderURL() and removing all parameters through setParameter(name, null) for each parameter name:",
+			renderURL.toString());
+
+		// set & delete parameters on render URL through setParameters() -
+
+		renderURL = renderResponse.createRenderURL();
+
+		renderURL.setParameter("privateRenderParameter1", "1");
+
+		parmMap = renderURL.getParameterMap();
+		keySet = parmMap.keySet();
+
+		String[] parmVals = { null };
+
+		for (String key : keySet) {
+
+			try {
+				parmMap.put(key, parmVals);
+			}
+			catch (Exception e) {
+				writer.write("updating map entry " + key + "failed.<br/>" + e.getMessage() + "<br/>");
+			}
+		}
+
+		try {
+			renderURL.setParameters(parmMap);
+		}
+		catch (Exception e) {
+			writer.write("setParameters() failed.<br/>" + e.getMessage() + "<br/>");
+		}
+
+		HTMLUtil.writeURL(writer,
+			"The following URL is a result of calling renderResponse.createRenderURL() and removing all parameters through setParameters()",
+			renderURL.toString());
+
+		// render URL with public render parameter removed -
+
+		renderURL = renderResponse.createRenderURL();
+
+		try {
+			renderURL.removePublicRenderParameter("publicRenderParameter1");
+		}
+		catch (Exception e) {
+			writer.write("remove publicRenderParameter1 from URL failed.<br/>" + e.getMessage() + "<br/>");
+		}
+
+		HTMLUtil.writeURL(writer,
+			"The following URL is a result of calling renderResponse.createRenderURL() and removePublicRenderParameter()",
+			renderURL.toString());
+
+		// Resource URL with no further processing -
 
 		ResourceURL resourceURL = renderResponse.createResourceURL();
 
-		// resourceURL.setParameter("publicRenderParameter1", (String) null); // IllegalArgumentException
-		resourceURL.setParameter("publicRenderParameter1", "3"); // IllegalArgumentException
-		writer.write(HTMLUtil.HR_TAG);
-		writer.write(
-			"The following URL is a result of calling renderResponse.createResourceURL() and then setParameter(\"publicRenderParameter1\", \"3\")");
-		writer.write(HTMLUtil.BR_TAG);
-		writer.write("<a href=\"");
-		writer.write(resourceURLAsString);
-		writer.write("\">");
-		writer.write(resourceURLAsString);
-		writer.write("</a>");
-		writer.write(HTMLUtil.HR_TAG);
+		HTMLUtil.writeURL(writer, "The following URL is a result of calling renderResponse.createResourceURL()",
+			resourceURL.toString());
+
+		// Resource URL attempting to remove parameters -
+
+		resourceURL = renderResponse.createResourceURL();
+
+		try {
+			resourceURL.setParameter("publicRenderParameter1", (String) null);
+		}
+		catch (Exception e) {
+			writer.write("remove publicRenderParameter1 from URL failed.<br/>" + e.getMessage() + "<br/>");
+		}
+
+		try {
+			resourceURL.setParameter("privateRenderParameter1", (String) null);
+		}
+		catch (Exception e) {
+			writer.write("remove privateRenderParameter1 from URL failed.<br/>" + e.getMessage() + "<br/>");
+		}
+
+		HTMLUtil.writeURL(writer,
+			"The following URL is a result of calling renderResponse.createResourceURL() and attempting to remove publicRenderParameter1 & privateRenderParameter1:",
+			resourceURL.toString());
+
+		// Resource URL setting parameters using various means -
+
+		resourceURL = renderResponse.createResourceURL();
+
+		resourceURL.setParameter("publicRenderParameter1", "30");
+		resourceURL.setParameter("privateRenderParameter1", "35");
+		resourceURL.setParameter("resourceURLParameter2", "40");
+
+		String[] values2 = { "Barney", "Betty" };
+		resourceURL.getParameterMap().put("resourceURLParameter3", values2);
+
+		HTMLUtil.writeURL(writer,
+			"The following URL is a result of calling renderResponse.createResourceURL() and setting resource parameters:",
+			resourceURL.toString());
 
 	}
+
 }
