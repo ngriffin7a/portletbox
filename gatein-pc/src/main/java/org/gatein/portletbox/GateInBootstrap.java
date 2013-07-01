a8dcaf785bb063cfe2062c6d70480424e5f3b866
Neil Griffin <neil.griffin.scm@gmail.com>
2013-07-01 16:41:48 -0400

    Switched from LGPL to BSD license

--- gatein-pc/src/main/java/org/gatein/portletbox/GateInBootstrap.java
+++ gatein-pc/src/main/java/org/gatein/portletbox/GateInBootstrap.java
@@ -1,3 +1,35 @@
+/**
+ * Software License Agreement (BSD License)
+ * 
+ * Copyright (c) 2013, Liferay Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use of this software in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above
+ *   copyright notice, this list of conditions and the
+ *   following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the
+ *   following disclaimer in the documentation and/or other
+ *   materials provided with the distribution.
+ * 
+ * * The name of Liferay Inc. may not be used to endorse or promote products
+ *   derived from this software without specific prior
+ *   written permission of Liferay Inc.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.gatein.portletbox;
 
 import java.net.URL;
@@ -16,57 +48,67 @@ import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathFactory;
 
 import org.gatein.pc.embed.EmbedServlet;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Text;
+
 
 /**
  * Implement the {@link ServletContainerInitializer} interface for embedding GateIn Portlet Container in a war file
  * containing portlets.
  *
- * @author Julien Viet
+ * @author  Julien Viet
  */
 public class GateInBootstrap implements ServletContainerInitializer {
 
-    @Override
-    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
-        ArrayList<String> portletNames = null;
-        try {
-            URL url = ctx.getResource("WEB-INF/portlet.xml");
+	@Override
+	public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+		ArrayList<String> portletNames = null;
 
-            //
-            if (url != null) {
-                // XPath to get the portlet names
-                portletNames = new ArrayList<String>();
-                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder builder = factory.newDocumentBuilder();
-                Document doc = builder.parse(url.openStream());
-                XPath xpath = XPathFactory.newInstance().newXPath();
-                XPathExpression expr = xpath.compile("//portlet-name/text()");
-                NodeList portletNameNodes = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
-                for (int i = 0;i < portletNameNodes.getLength();i++) {
-                    Text portletNameNode = (Text) portletNameNodes.item(i);
-                    portletNames.add(portletNameNode.getWholeText().trim());
-                }
-            } else {
-                ctx.log("No portlets inside this web application");
-            }
+		try {
+			URL url = ctx.getResource("WEB-INF/portlet.xml");
 
-        } catch (Exception e) {
-            ctx.log("Could not obtain a valid portlet.xml file", e);
-        }
+			//
+			if (url != null) {
 
-        //
-        if (portletNames != null) {
-            ctx.log("Detected portlet XML file with portlets " + portletNames + " -> Bootstrapping GateIn Portlet Container");
+				// XPath to get the portlet names
+				portletNames = new ArrayList<String>();
 
-            //
-            ServletRegistration.Dynamic embedRegistration = ctx.addServlet("EmbedServlet", EmbedServlet.class);
-            embedRegistration.addMapping("/embed/*");
+				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder builder = factory.newDocumentBuilder();
+				Document doc = builder.parse(url.openStream());
+				XPath xpath = XPathFactory.newInstance().newXPath();
+				XPathExpression expr = xpath.compile("//portlet-name/text()");
+				NodeList portletNameNodes = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
 
-            //
-            ServletRegistration.Dynamic redirectRegistration = ctx.addServlet("RedirectServlet", new RedirectServlet(portletNames));
-            redirectRegistration.addMapping("/");
-        }
-    }
+				for (int i = 0; i < portletNameNodes.getLength(); i++) {
+					Text portletNameNode = (Text) portletNameNodes.item(i);
+					portletNames.add(portletNameNode.getWholeText().trim());
+				}
+			}
+			else {
+				ctx.log("No portlets inside this web application");
+			}
+
+		}
+		catch (Exception e) {
+			ctx.log("Could not obtain a valid portlet.xml file", e);
+		}
+
+		//
+		if (portletNames != null) {
+			ctx.log("Detected portlet XML file with portlets " + portletNames +
+				" -> Bootstrapping GateIn Portlet Container");
+
+			//
+			ServletRegistration.Dynamic embedRegistration = ctx.addServlet("EmbedServlet", EmbedServlet.class);
+			embedRegistration.addMapping("/embed/*");
+
+			//
+			ServletRegistration.Dynamic redirectRegistration = ctx.addServlet("RedirectServlet",
+					new RedirectServlet(portletNames));
+			redirectRegistration.addMapping("/");
+		}
+	}
 }
--- gatein-pc/src/main/java/org/gatein/portletbox/RedirectServlet.java
+++ gatein-pc/src/main/java/org/gatein/portletbox/RedirectServlet.java
@@ -1,3 +1,35 @@
+/**
+ * Software License Agreement (BSD License)
+ * 
+ * Copyright (c) 2013, Liferay Inc.
+ * All rights reserved.
+ * 
+ * Redistribution and use of this software in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above
+ *   copyright notice, this list of conditions and the
+ *   following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the
+ *   following disclaimer in the documentation and/or other
+ *   materials provided with the distribution.
+ * 
+ * * The name of Liferay Inc. may not be used to endorse or promote products
+ *   derived from this software without specific prior
+ *   written permission of Liferay Inc.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.gatein.portletbox;
 
 import java.io.IOException;
@@ -8,35 +40,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
- * This servlet redirects to the embed portlet container initialized with the known portlets in this application.
- * It is declared as a default servlet (i.e on '/') and is matched when no other servlet is matched.
+ * This servlet redirects to the embed portlet container initialized with the known portlets in this application. It is
+ * declared as a default servlet (i.e on '/') and is matched when no other servlet is matched.
  *
- * @author Julien Viet
+ * @author  Julien Viet
  */
 public class RedirectServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 4802312414356615240L;
 
-    private final List<String> portletNames;
+	private final List<String> portletNames;
 
-    public RedirectServlet(List<String> portletNames) {
-        this.portletNames = portletNames;
-    }
+	public RedirectServlet(List<String> portletNames) {
+		this.portletNames = portletNames;
+	}
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        //
-        StringBuilder buffer = new StringBuilder();
-        buffer.append(req.getContextPath());
-        buffer.append("/embed");
-        for (String portletName : portletNames) {
-            // We suppose the portlet names don't contain chars we need to encode
-            buffer.append('/').append(portletName);
-        }
+		//
+		StringBuilder buffer = new StringBuilder();
+		buffer.append(req.getContextPath());
+		buffer.append("/embed");
 
-        // Redirect
-        resp.sendRedirect(buffer.toString());
-    }
+		for (String portletName : portletNames) {
+
+			// We suppose the portlet names don't contain chars we need to encode
+			buffer.append('/').append(portletName);
+		}
+
+		// Redirect
+		resp.sendRedirect(buffer.toString());
+	}
 }
